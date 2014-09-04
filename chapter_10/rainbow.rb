b7d1f12dfe1e46839fc33c0d65a4deb96d859c46
Greg Cosgrove <gregcosgrove@gmail.com>
2014-09-04 17:26:50 -0400

    Rename folders

---
+++ chapter_10/rainbow.rb
@@ -0,0 +1,12 @@
+class Rainbow
+  include Enumberable
+  def each
+    yield "red"
+    yield "orange"
+    yield "yellow"
+    yield "green"
+    yield "blue"
+    yield "indigo"
+    yield "violet"
+  end
+end
